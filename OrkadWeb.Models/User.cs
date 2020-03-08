2a7ed931b834a0c933b6e87597edff535341560d
Orkad <nicolas.gidon.43@gmail.com>
2020-03-08 22:29:43 +0100

    Création d'un modèle simple d'utilisateur

---
+++ OrkadWeb.Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OrkadWeb.Models
+{
+    /// <summary>
+    /// Représente un utilisateur de "OrkadWeb"
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Identifiant unique de l'utilisateur
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Nom d'utilisateur
+        /// </summary>
+        public virtual string Username { get; set; }
+
+        /// <summary>
+        /// Mot de passe de l'utilisateur (crypté)
+        /// </summary>
+        public virtual string Password { get; set; }
+
+        /// <summary>
+        /// Adresse email de contact de l'utilisateur
+        /// </summary>
+        public virtual string Email { get; set; }
+    }
+}
