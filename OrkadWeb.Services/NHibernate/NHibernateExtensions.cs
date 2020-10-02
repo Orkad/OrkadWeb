7fda032a951df58f1ecbfbb4040d1644aabdfb7e
Orkad <nicolas.gidon.43@gmail.com>
2020-10-02 23:28:11 +0200

    test

--- OrkadWeb.Services/NHibernate/NHibernateExtensions.cs
+++ OrkadWeb.Services/NHibernate/NHibernateExtensions.cs
@@ -1,8 +1,14 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using Microsoft.Extensions.DependencyInjection;
+using NHibernate.Cfg;
 using OrkadWeb.Models.Enums.NHibernate;
 using OrkadWeb.Services.NHibernate.Mapping;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 
 namespace OrkadWeb.Services.NHibernate
 {
@@ -10,14 +16,80 @@ namespace OrkadWeb.Services.NHibernate
     {
         public static IServiceCollection AddNHibernate(this IServiceCollection services, string connectionString)
         {
-            var sessionFactory = Fluently
+            // check cache
+            var nhConfigCache = new NHibernateConfigurationCache(Assembly.GetAssembly(typeof(UserMap)));
+            var config = nhConfigCache.LoadConfigurationFromFile();
+            if (config == null)
+            {
+                var fluentConfiguration = Fluently
                 .Configure()
                 .Database(MySQLConfiguration.Standard.ConnectionString(connectionString))
-                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>().Conventions.Add<EnumConvention>())
-                .BuildSessionFactory();
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>().Conventions.Add<EnumConvention>()); // 132ms
+                config = fluentConfiguration.BuildConfiguration(); // 549ms
+                nhConfigCache.SaveConfigurationToFile(config);
+            }
+            
+            var sessionFactory = config.BuildSessionFactory(); // 539ms
+
             services.AddSingleton(sessionFactory);
-            services.AddScoped(factory => sessionFactory.OpenSession());
+            services.AddScoped(serviceProvider => sessionFactory.OpenSession());
+
             return services;
+        }
+    }
+
+    public class NHibernateConfigurationCache
+    {
+        private readonly string _cacheFile;
+        private readonly Assembly _definitionsAssembly;
+
+        public NHibernateConfigurationCache(Assembly definitionsAssembly)
+        {
+            _definitionsAssembly = definitionsAssembly;
+            _cacheFile = "nh.cfg";
+        }
+
+        public void DeleteCacheFile()
+        {
+            if (File.Exists(_cacheFile))
+                File.Delete(_cacheFile);
+        }
+
+        public bool IsConfigurationFileValid
+        {
+            get
+            {
+                if (!File.Exists(_cacheFile))
+                    return false;
+                var configInfo = new FileInfo(_cacheFile);
+                var asmInfo = new FileInfo(_definitionsAssembly.Location);
+
+                if (configInfo.Length < 5 * 1024)
+                    return false;
+
+                return configInfo.LastWriteTime >= asmInfo.LastWriteTime;
+            }
+        }
+
+        public void SaveConfigurationToFile(Configuration configuration)
+        {
+            using (var file = File.Open(_cacheFile, FileMode.Create))
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(file, configuration);
+            }
+        }
+
+        public Configuration LoadConfigurationFromFile()
+        {
+            if (!IsConfigurationFileValid)
+                return null;
+
+            using (var file = File.Open(_cacheFile, FileMode.Open, FileAccess.Read))
+            {
+                var bf = new BinaryFormatter();
+                return bf.Deserialize(file) as Configuration;
+            }
         }
     }
 }
