958c2dba969fd71e05b89c3b5a1b8a6627ec8579
nicolas gidon <nicolas.gidon.43@gmail.com>
2021-11-27 07:59:12 +0100

    Register command

---
+++ OrkadWeb.Logic/Common/Response.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrkadWeb.Logic.Common
+{
+    /// <summary>
+    /// Représente une réponse coté métier
+    /// </summary>
+    public class Response
+    {
+        /// <summary>
+        /// Constructeur d'une réponse d'erreur
+        /// </summary>
+        /// <param name="errorMessage">Message d'erreur correpondant</param>
+        public Response(string errorMessage)
+        {
+            Success = false;
+            Error = errorMessage;
+        }
+
+        public Response()
+        {
+
+        }
+
+        /// <summary>
+        /// Détermine que l'opération s'est déroulée avec succès
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Message d'erreur lorsque un problème est rencontré
+        /// </summary>
+        public string Error { get; set; }
+    }
+}
