c09e2eabd656fd274803b34d4959cafa8127bc1a
nicolas gidon <nicolas.gidon.43@gmail.com>
2021-11-30 08:48:42 +0100

    Partage feature

---
+++ OrkadWeb.Tests/Features/Partage.feature
@@ -0,0 +1,17 @@
+﻿Fonctionnalité: Partage
+Expose la fonctionnalité de base de partage de dépenses
+
+Contexte: 
+	Etant donné que il existe les utilisateurs suivants :
+	| Nom d'utilisateur |
+	| Nicolas           |
+	| Léa               |
+	Et qu'il existe un partage entre Nicolas et Léa
+	
+Scénario: Affichage des informations principales sur le partage
+	Quand Nicolas ajoute la dépense de 30€ nommée "chocolat" au partage
+	Et que Léa ajoute la dépense de 50€ nommée "gateau" au partage
+	Alors le partage affiche que les dépenses totales s'élèvent à hauteur de 80€
+	Et le partage affiche qu'il y a un écart de 20€
+	Et Nicolas doit 10€ à Léa sur le partage
+	
---
+++ OrkadWeb.Tests/Steps/PartageStepDefinitions.cs
@@ -0,0 +1,70 @@
+﻿using OrkadWeb.Data;
+using OrkadWeb.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace OrkadWeb.Tests.Steps
+{
+    /// <summary>
+    /// Contexte des objets nommés
+    /// </summary>
+    public class NamedContext
+    {
+        public Dictionary<string, User> Users { get; } = new Dictionary<string, User>();
+    }
+
+    public class LastContext
+    {
+
+    }
+
+    [Binding]
+    public class PartageStepDefinitions
+    {
+        private readonly NamedContext namedContext;
+
+        public PartageStepDefinitions(IDataService dataService, NamedContext namedContext)
+        {
+            this.namedContext = namedContext;
+        }
+
+        [Given(@"il existe les utilisateurs suivants :")]
+        public void GivenExistsUsers(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"il existe un partage entre (.*) et (.*)")]
+        public void GivenExistsShare(string user1, string user2)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"(.*) ajoute la dépense de (.*)€ nommée ""([^""]*)"" au partage")]
+        public void WhenUserAddExpense(string user, decimal amount, string name)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"le partage affiche que les dépenses totales s'élèvent à hauteur de (.*)€")]
+        public void ThenShareTotalAmountIs(decimal amount)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"le partage affiche qu'il y a un écart de (.*)€")]
+        public void ThenShareGapIs(decimal amout)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"(.*) doit (.*)€ à (.*) sur le partage")]
+        public void ThenUserOweToOtherOnShare(string user1, decimal amount, string user2)
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
