37eebbe8b2253f1736709e0894beb4cb126a5035
nicolas gidon <nicolas.gidon.43@gmail.com>
2023-02-24 09:07:08 +0100

    new hosting model

--- OrkadWeb.Angular/Program.cs
+++ OrkadWeb.Angular/Program.cs
@@ -1,26 +1,128 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using FluentMigrator.Runner;
+using Hangfire;
+using Hangfire.SqlServer;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OrkadWeb.Angular.Config;
+using OrkadWeb.Angular.Hubs;
+using OrkadWeb.Angular.Models;
+using OrkadWeb.Application;
+using OrkadWeb.Application.Users;
+using OrkadWeb.Domain.Extensions;
+using OrkadWeb.Infrastructure;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Security.Claims;
 
-namespace OrkadWeb.Angular
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+// MVC
+services.AddControllersWithViews();
+// ANGULAR SPA
+services.AddSpaStaticFiles(configuration =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    configuration.RootPath = "ClientApp/dist/client-app";
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+// AUTHENTICATION
+services.AddSingleton<JwtConfig>();
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer();
+services.ConfigureOptions<JwtConfig>();
+services.AddSingleton<IIdentityTokenGenerator, JwtTokenGenerator>();
+services.AddSession();
+services.AddHttpContextAccessor();
+services.AddScoped<IAuthenticatedUser>(ResolveAuthenticatedUser);
+AuthenticatedUser ResolveAuthenticatedUser(IServiceProvider serviceProvider)
+{
+    var user = serviceProvider.GetService<IHttpContextAccessor>().HttpContext.User;
+    if (user.Identity.IsAuthenticated)
+    {
+        return new AuthenticatedUser
+        {
+            Id = int.Parse(user.FindFirst(ClaimTypes.NameIdentifier).Value),
+            Name = user.FindFirst(ClaimTypes.Name).Value,
+            Email = user.FindFirst(ClaimTypes.Email).Value,
+        };
     }
+    return null;
 }
+
+// APPLICATION + INFRASTRUCTURE
+services.AddApplicationServices();
+services.AddInfrastructureServices(configuration);
+
+// HANGFIRE
+services.AddHangfire(h => h
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(configuration.GetRequiredValue("ConnectionStrings:Hangfire"), new SqlServerStorageOptions
+    {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true
+    }));
+services.AddHangfireServer();
+
+// SIGNALR
+services.AddSignalR();
+services.AddMediatR(Assembly.GetExecutingAssembly());
+
+var app = builder.Build();
+var dev = app.Environment.IsDevelopment();
+var prod = !dev;
+if (dev)
+{
+    app.UseDeveloperExceptionPage();
+}
+if (prod)
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+if (prod)
+{
+    app.UseSpaStaticFiles();
+}
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+    // hangfire
+    endpoints.MapHangfireDashboard();
+    // signalr
+    endpoints.MapHub<NotificationHub>("/hub/notification");
+});
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    if (dev)
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+    }
+});
+// HANGFIRE
+app.UseHangfireDashboard();
+
+// DATABASE MIGRATION
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetService<IMigrationRunner>().MigrateUp();
+}
+
+app.Run();
