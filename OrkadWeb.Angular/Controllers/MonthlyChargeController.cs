1309af69f6e503281045ec3316584236480c4513
nicolas gidon <nicolas.gidon.43@gmail.com>
2023-04-01 17:30:11 +0200

    monthly transaction refactoring

---
+++ OrkadWeb.Angular/Controllers/MonthlyChargeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrkadWeb.Application.MonthlyTransactions.Commands;
+using OrkadWeb.Application.MonthlyTransactions.Models;
+using OrkadWeb.Application.MonthlyTransactions.Queries;
+
+namespace OrkadWeb.Angular.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/monthly/charges")]
+public class MonthlyChargeController : ControllerBase
+{
+    private readonly ISender sender;
+
+    public MonthlyChargeController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IEnumerable<MonthlyChargeVM>> GetCharges(CancellationToken cancellationToken)
+    {
+        return await sender.Send(new GetChargesQuery(), cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<int> AddCharge([FromBody] MonthlyChargeVM vm, CancellationToken cancellationToken)
+    {
+        return await sender.Send(new AddChargeCommand
+        {
+            Amount = vm.Amount,
+            Name = vm.Name
+        }, cancellationToken);
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task EditCharge(int id, [FromBody] MonthlyChargeVM vm, CancellationToken cancellationToken)
+    {
+        if (id <= 0 || vm?.Id != id) throw new BadHttpRequestException("identifiant incorrect");
+        await sender.Send(new EditChargeCommand
+        {
+            Id = id,
+            Amount = vm.Amount,
+            Name = vm.Name
+        }, cancellationToken);
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task DeleteCharge(int id, CancellationToken cancellationToken)
+    {
+        await sender.Send(new DeleteChargeCommand { Id = id }, cancellationToken);
+    }
+}
---
+++ OrkadWeb.Angular/Controllers/MonthlyIncomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrkadWeb.Application.MonthlyTransactions.Commands;
+using OrkadWeb.Application.MonthlyTransactions.Models;
+using OrkadWeb.Application.MonthlyTransactions.Queries;
+
+namespace OrkadWeb.Angular.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/monthly/incomes")]
+public class MonthlyIncomeController : ControllerBase
+{
+    private readonly ISender sender;
+
+    public MonthlyIncomeController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IEnumerable<MonthlyIncomeVM>> GetIncomes(CancellationToken cancellationToken)
+    {
+        return await sender.Send(new GetIncomesQuery(), cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<int> AddIncome([FromBody] MonthlyIncomeVM vm, CancellationToken cancellationToken)
+    {
+        return await sender.Send(new AddIncomeCommand
+        {
+            Amount = vm.Amount,
+            Name = vm.Name
+        }, cancellationToken);
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task EditIncome(int id, [FromBody] MonthlyIncomeVM vm, CancellationToken cancellationToken)
+    {
+        if (id <= 0 || vm?.Id != id) throw new BadHttpRequestException("identifiant incorrect");
+        await sender.Send(new EditIncomeCommand
+        {
+            Id = id,
+            Amount = vm.Amount,
+            Name = vm.Name
+        }, cancellationToken);
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task DeleteIncome(int id, CancellationToken cancellationToken)
+    {
+        await sender.Send(new DeleteIncomeCommand { Id = id }, cancellationToken);
+    }
+}
