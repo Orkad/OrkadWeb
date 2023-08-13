5e0a19c3a92e48c952e404dc3be4ff3ad58221a2
nicolas gidon <nicolas.gidon.43@gmail.com>
2023-08-13 20:43:59 +0200

    OwnableInterceptor.cs save OK

--- OrkadWeb.Infrastructure/Persistence/Interceptors/OwnableInterceptor.cs
+++ OrkadWeb.Infrastructure/Persistence/Interceptors/OwnableInterceptor.cs
@@ -1,6 +1,11 @@
-﻿using NHibernate;
+﻿using System;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Persister.Entity;
 using NHibernate.Type;
 using OrkadWeb.Application.Users;
+using OrkadWeb.Domain.Entities;
+using OrkadWeb.Domain.Primitives;
 
 namespace OrkadWeb.Infrastructure.Persistence.Interceptors;
 
@@ -12,7 +17,7 @@ public class OwnableInterceptor : EmptyInterceptor
     {
         this.user = user;
     }
-    
+
     public override bool OnLoad(object entity, object id, object[] state, string[] propertyNames, IType[] types)
     {
         return base.OnLoad(entity, id, state, propertyNames, types);
@@ -20,12 +25,22 @@ public class OwnableInterceptor : EmptyInterceptor
 
     public override bool OnSave(object entity, object id, object[] state, string[] propertyNames, IType[] types)
     {
-        return base.OnSave(entity, id, state, propertyNames, types);
+        if (entity is not IOwnable) return false;
+        SetValue(state, propertyNames, nameof(IOwnable.Owner), new User { Id = user.Id });
+        return true;
     }
 
-    public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames,
+    public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState,
+        string[] propertyNames,
         IType[] types)
     {
         return base.OnFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+
+    private void SetValue(object[] state, string[] propertyNames, string propertyName, object value)
+    {
+        var index = Array.IndexOf(propertyNames, propertyName);
+        ;
+        state[index] = value;
     }
 }
---
+++ tests/OrkadWeb.Tests/Persistence/OwnableInterceptorTests.cs
@@ -0,0 +1,118 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using OrkadWeb.Application.Users;
+using OrkadWeb.Domain.Consts;
+using OrkadWeb.Domain.Entities;
+using OrkadWeb.Domain.Primitives;
+using OrkadWeb.Infrastructure;
+using OrkadWeb.Infrastructure.Persistence.Interceptors;
+using OrkadWeb.Infrastructure.Persistence.Mapping;
+
+namespace OrkadWeb.Tests.Persistence;
+
+[TestClass]
+public class OwnableInterceptorTests
+{
+    private ISession selfSession;
+    private ISession otherSession;
+    private ISessionFactory sessionFactory;
+
+    private class OwnableEntity : IOwnable
+    {
+        public virtual int Id { get; set; }
+        public virtual User Owner { get; }
+    }
+
+    private class OwnableEntityMap : ClassMap<OwnableEntity>
+    {
+        public OwnableEntityMap()
+        {
+            Id(x => x.Id).GeneratedBy.Assigned();
+            References(x => x.Owner);
+        }
+    }
+
+    private class OtherUser : IAppUser
+    {
+        public int Id { get; } = 999;
+        public string Name { get; } = "Other";
+        public string Email { get; } = "other@email.com";
+        public string Role { get; } = UserRoles.User;
+    }
+
+    private class SelfUser : IAppUser
+    {
+        public int Id { get; } = 1;
+        public string Name { get; } = "Self";
+        public string Email { get; } = "self@email.com";
+        public string Role { get; } = UserRoles.User;
+    }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        var configuration = Fluently.Configure()
+            .Database(SQLiteConfiguration.Standard.InMemory())
+            .Mappings(mapping => mapping.FluentMappings.Add<OwnableEntityMap>().Add<UserMap>())
+            .BuildConfiguration();
+        sessionFactory = configuration.BuildSessionFactory();
+        var selfUser = new SelfUser();
+        var otherUser = new OtherUser();
+        selfSession = sessionFactory
+            .WithOptions().Interceptor(new OwnableInterceptor(selfUser))
+            .OpenSession();
+        otherSession = sessionFactory
+            .WithOptions().Interceptor(new OwnableInterceptor(otherUser)).Connection(selfSession.Connection)
+            .OpenSession();
+        new SchemaExport(configuration).Execute(false, true, false, selfSession.Connection, null);
+        selfSession.Save(new User
+        {
+            Id = selfUser.Id,
+            Username = selfUser.Name,
+            Email = selfUser.Email,
+            Role = selfUser.Role,
+            Password = "self",
+        });
+        otherSession.Save(new User
+        {
+            Id = otherUser.Id,
+            Username = otherUser.Name,
+            Email = otherUser.Email,
+            Role = otherUser.Role,
+            Password = "other"
+        });
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        otherSession.Dispose();
+        selfSession.Dispose();
+        sessionFactory.Dispose();
+    }
+
+    [TestMethod]
+    public void ShouldAutoAffectOwnerTest()
+    {
+        var ownedEntity = new OwnableEntity();
+        selfSession.Save(ownedEntity);
+        Check.That(ownedEntity.Owner).IsNotNull();
+        Check.That(ownedEntity.Owner.Id).IsEqualTo(1);
+    }
+
+    [TestMethod]
+    public void ShouldDenyUpdateFromOthersTest()
+    {
+        var ownedEntity = new OwnableEntity
+        {
+            Id = 1,
+        };
+        selfSession.Save(ownedEntity);
+        var notOwnedEntity = otherSession.Get<OwnableEntity>(1);
+        Check.ThatCode(() => otherSession);
+    }
+}
